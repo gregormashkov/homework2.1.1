49d8e293b4a7ebf66ad8f51e7b12841302427f81
grifo <grifoncheg1996@gmail.com>
2022-11-28 16:04:00 +0300

    /

---
+++ src/human.java
@@ -0,0 +1,24 @@
+public class human {
+    String name;
+    String city;
+    String position;
+    int age;
+
+
+    String name1 = "Максим";
+    String city1 = "Минск";
+    String position1 = "бренд-менеджер";
+    int age1 = 35;
+    String name2 = "Аня";
+    String city2 = "Москва";
+    String position2 = "методист образовательных программ";
+    int age2 = 29;
+    String name3 = "Катя";
+    String city3 = "Калиниград";
+    String position3 = "продакт-менеджер";
+    int age3 = 28;
+    String name4 = "Артём";
+    String position4 = "директор по развитию бизнеса";
+    int age4 = 27;
+
+}
